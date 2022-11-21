61fbdd9f514eafa62337a2548d36c50551e632ae
albertoalmenara <PeopleRK>
2022-11-21 11:43:42 +0100

    Sprint 8 / Ejercicio 10

--- src/app/content/auth/login/user.interface.ts
+++ src/app/content/auth/login/user.interface.ts
@@ -1,6 +1,6 @@
 export interface User {
     name:       string | null | undefined;
-    surname:    number | null | undefined;
+    surname:    string | null | undefined;
     email:      string | null | undefined;
     password:   string | null | undefined;
     active:     boolean | null | undefined;
